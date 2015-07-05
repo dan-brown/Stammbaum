0248185cb01df083cda7a507af55c9d9da1f966b
dan-brown <daniel.brown@live.de>
2015-07-05 11:56:41 +0200

    - validierung stimmt - personen-objekt wird bei 'cancel' korrekt zurückgesetz

--- Implementierung/familytree.js
+++ Implementierung/familytree.js
@@ -3,6 +3,7 @@ var xml;
 var persons;
 var inlaws;
 var person;
+var person_backup;
 
 var request;
 
@@ -17,11 +18,11 @@ function init(){
     request.onreadystatechange = request_onreadystatechange;
     updateXML();
 
-    $(document).keydown(function(e){if (shown && e.keyCode == 27) hideInfobox();});
+    $(document).keydown(function(e){if (shown && e.keyCode == 27) cancelInfobox();});
     $("#infobox_submit").click(submit);
-    $("#infobox_background").click(hideInfobox);
-    $("#infobox_cancel").click(hideInfobox);
-    hideInfobox();
+    $("#infobox_background").click(cancelInfobox);
+    $("#infobox_cancel").click(cancelInfobox);
+    closeInfobox();
 
     $(".member").click(showInfobox);
     $(".inlaw").click(showInfobox);
@@ -43,9 +44,19 @@ function request_onreadystatechange() {
     inlaws = xml.getElementsByTagName("inlaw");
 }
 
-function hideInfobox(){
+function cancelInfobox() {
+    $(person).attr("forename", $(person_backup).attr("forename"))
+    $(person).attr("surname", $(person_backup).attr("surname"))
+    $(person).attr("sex", $(person_backup).attr("sex"))
+    $(person).attr("birth_date", $(person_backup).attr("birth_date"))
+    $(person).attr("death_date", $(person_backup).attr("death_date"))
+    closeInfobox();
+}
+
+function closeInfobox(){
     shown = false;
     $("#infobox").hide();
+    $(".info_input").removeClass("invalid");
 }
 
 function showInfobox(){
@@ -59,21 +70,22 @@ function showInfobox(){
 
 function updatePerson(pid) {
     for ( var i = 0; i < persons.length + inlaws.length; i++ ) {
-        var cur = ( i < persons.length ) ? persons.item(i) : inlaws.item(i-persons.length);
-        if($(cur).attr("pid") == pid) {
-            person = cur;
+        var person_i = ( i < persons.length ) ? persons.item(i) : inlaws.item(i-persons.length);
+        if($(person_i).attr("pid") === pid) {
+            person = person_i;
+            person_backup = person_i;
             break;
         }
     }
 }
 
 function updateInfobox(){
-    $("#input_forename").value = $(person).attr("forename");
-    $("#input_surname").value = $(person).attr("surname");
-    if($(person).attr("sex") === "M") $("#input_sex_male").checked = true;
-    else $("#input_sex_female").checked = true;
-    $("#input_birth_date").value = toLocaleDateString($(person).attr("birth_date"));
-    $("#input_death_date").value = toLocaleDateString($(person).attr("death_date"));
+    $("#input_forename").val($(person).attr("forename"));
+    $("#input_surname").val($(person).attr("surname"));
+    if($(person).attr("sex") === "M") $("#input_sex_male").prop("checked","checked");
+    else $("#input_sex_female").prop("checked","checked");
+    $("#input_birth_date").val(toLocaleDateString($(person).attr("birth_date")));
+    $("#input_death_date").val(toLocaleDateString($(person).attr("death_date")));
 }
 
 function toLocaleDateString(iso_text){
@@ -83,21 +95,22 @@ function toLocaleDateString(iso_text){
 
 
 function submit(){
-    var forename    = $("#input_forename").value;
-    var surname     = $("input_surname").value;
-    var sex         = $("#input_sex_male").checked ? "M" : "F";
-    var birth_date  = $("#input_birth_date").value;
-    var death_date  = $("#input_death_date").value;
+    var forename    = $("#input_forename").val();
+    var surname     = $("#input_surname").val();
+    var sex         = $("#input_sex_male").prop("checked") ? "M" : "F";
+    var birth_date  = $("#input_birth_date").val();
+    var death_date  = $("#input_death_date").val();
 
-    if (!validate(forename, birth_date, death_date)) return;
+    if (!validate(forename, birth_date, death_date)){
+        person = person_backup;
+        return;
+    }
 
     $(person).attr("forename", forename);
     $(person).attr("surname", surname);
     $(person).attr("sex", sex);
     $(person).attr("birth_date", birth_date);
     $(person).attr("death_date", death_date);
-
-    saveXML();
 }
 
 function validate(forename, birth_date, death_date){
@@ -105,37 +118,27 @@ function validate(forename, birth_date, death_date){
     if(forename === ""){
         $(input_forename).addClass("invalid");
         valid = false;
-        console.log("invalid: empty forename");
     }
-    console.log("bd");
     if(!validateDate(birth_date)){
         $("#input_birth_date").addClass("invalid");
         valid = false;
-        console.log("invalid birth_date: "+birth_date);
     }
     if(!validateDate(death_date)){
         $(input_birth_date).addClass("invalid");
         valid = false;
-        console.log("invalid death_date: "+death_date);
     }
-    console.log(valid);
     return valid;
 }
 
 function validateDate(locale_text) {
     if(locale_text === "") return true;
-    console.log(locale_text);
 
     var comps = locale_text.split(".");
     var d = parseInt(comps[0], 10);
     var m = parseInt(comps[1], 10);
     var y = parseInt(comps[2], 10);
-    console.log("d: " + d + ", m: " + m + ", y: " + y);
 
     var date = new Date(y, m - 1, d);
-    console.log("date.y:"+date.getFullYear()+" | txt:"+y);
-    console.log("date.m: "+date.getMonth()+" | txt:"+m);
-    console.log("date.d: "+date.getDate()+" | txt:"+d);
 
     return date.getFullYear() == y && date.getMonth() + 1 == m && date.getDate() == d;
 }
